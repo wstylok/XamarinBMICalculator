7b2bcb987e45207ebf3e79ac8b9b863557162c84
Wojciech Stylok <wojciech@stylok.pl>
2019-03-26 23:30:29 +0100

    Initial project version

--- BMIcalculator/BMIcalculator/MainPage.xaml.cs
+++ BMIcalculator/BMIcalculator/MainPage.xaml.cs
@@ -13,5 +13,39 @@ namespace BMIcalculator
         {
             InitializeComponent();
         }
+
+        async void Oblicz_Clicked(object sender, EventArgs e)
+        {
+            double x = 0;
+            double y = 0;
+            if(double.TryParse(Weight.Text, out x))
+            {
+                if(double.TryParse(Height.Text, out y))
+                {
+                    double bmi = double.Parse(Weight.Text) / (Math.Pow(double.Parse(Height.Text) / 100, 2));
+                    Wynik.BindingContext = bmi;
+                    if (bmi < 18.5)
+                    {
+                        CoOznacza.BindingContext = "Niedowaga";
+                    }
+                    else if (bmi >= 25)
+                    {
+                        CoOznacza.BindingContext = "Nadwaga";
+                    }
+                    else
+                    {
+                        CoOznacza.BindingContext = "Waga prawidłowa";
+                    }
+                }
+                else
+                {
+                    await DisplayAlert("Uwaga", "Podany wzrost jest niepoprawny", "OK");
+                }
+            }
+            else
+            {
+                await DisplayAlert("Uwaga", "Podana waga jest niepoprawna", "OK");
+            }
+        }
     }
 }
